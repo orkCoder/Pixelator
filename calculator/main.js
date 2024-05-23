79955f726569d12a7380f13a03c23bc69b65c29d
Mailwork <144586372+MailWork256@users.noreply.github.com>
2024-05-23 20:27:00 +0100

    better asesthethics

--- calculator/main.js
+++ calculator/main.js
@@ -1,35 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const input = document.getElementById("display");
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.getElementById("display");
 
-    const numbers = {
-        one: document.getElementById("one"),
-        two: document.getElementById("two"),
-        three: document.getElementById("three"),
-        four: document.getElementById("four"),
-        five: document.getElementById("five"),
-        six: document.getElementById("six"),
-        seven: document.getElementById("seven"),
-        eight: document.getElementById("eight"),
-        nine: document.getElementById("nine"),
-        zero: document.getElementById("zero")
-    }
-    
-    const operators = {
-        plus: document.getElementById("plus"),
-        minus: document.getElementById("minus"),
-        times: document.getElementById("times"),
-        divide: document.getElementById("divide")
-    }
-    
-    const erasers = {
-        clearAll: document.getElementById("clearAll"),
-        clear: document.getElementById("clear")
-    }
+  const numbers = {
+    one: document.getElementById("one"),
+    two: document.getElementById("two"),
+    three: document.getElementById("three"),
+    four: document.getElementById("four"),
+    five: document.getElementById("five"),
+    six: document.getElementById("six"),
+    seven: document.getElementById("seven"),
+    eight: document.getElementById("eight"),
+    nine: document.getElementById("nine"),
+    zero: document.getElementById("zero"),
+  };
 
-    let array = [];
+  const operators = {
+    plus: document.getElementById("plus"),
+    minus: document.getElementById("minus"),
+    times: document.getElementById("times"),
+    divide: document.getElementById("divide"),
+  };
 
-    numbers.one.addEventListener("click", function() {
-        console.log("Button one click")
-        input.value += 1
-    })
-})
+  const erasers = {
+    clearAll: document.getElementById("clearAll"),
+    clear: document.getElementById("clear"),
+  };
+
+  let array = [];
+
+  numbers.one.addEventListener("click", function () {
+    console.log("Button one click");
+    input.value += 1;
+  });
+});
